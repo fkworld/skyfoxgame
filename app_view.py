06442b92aabfad86ef511d8ddc44b290564e5d66
fengyong <fkworld@foxmail.com>
2017-07-10 11:37:35 +0800

    add other 2 apps‘

--- app_view.py
+++ app_view.py
@@ -4,8 +4,16 @@ app_view = Blueprint('app_view', __name__, template_folder='templates')
 
 @app_view.route('/', methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    return render_template('app_index.html')
 
 @app_view.route('/flick_bird')
 def flick_bird():
     return render_template('app_flick_bird.html')
+
+@app_view.route('/walking_aliens')
+def walking_aliens():
+    return render_template('app_walking_aliens.html')
+
+@app_view.route('/magic_invert')
+def magic_invert():
+    return render_template('app_magic_invert.html')
