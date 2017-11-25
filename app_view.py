07edfa086c66dcb2bd8d8f887428605f04a0da5a
fkworld <fkworld@foxmail.com>
2017-11-25 17:37:40 +0800

    清理appview

--- app_view.py
+++ app_view.py
@@ -9,23 +9,3 @@ def index():
     app = App()
     apps = app.search_all()
     return render_template('/app_index.html',apps=apps)
-
-@app_view.route('/flick_bird')
-def flick_bird():
-    return render_template('/app/flick_bird/flick_bird.html')
-
-@app_view.route('/magic_invert')
-def magic_invert():
-    return render_template('/app/magic_invert/magic_invert.html')
-
-@app_view.route('/walking_alien')
-def walking_alien():
-    return render_template('/app/walking_alien/walking_alien.html')
-
-@app_view.route('/girls_foosball')
-def girls_foosball():
-    return render_template('/app/girls_foosball/girls_foosball.html')
-
-@app_view.route('/back_to_back')
-def back_to_back():
-    return render_template('/app/back_to_back/back_to_back.html')
